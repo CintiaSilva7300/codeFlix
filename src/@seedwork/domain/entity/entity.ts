9cc9ce1aff586a220842078496174db65d1565e7
cintia <cintiasilva7300@gmail.com>
2023-02-12 14:44:26 -0300

    teste inMemoryReposytory

--- src/@seedwork/domain/entity/entity.ts
+++ src/@seedwork/domain/entity/entity.ts
@@ -1,6 +1,6 @@
 import UniqueEntityID from "../value-objects/unique-entity-id.vo";
 
-export default abstract class Entity<Props> {
+export default abstract class Entity<Props = any> {
     public readonly uniqueEntityId: UniqueEntityID
 
     constructor(public readonly props: Props, id?: UniqueEntityID) {
